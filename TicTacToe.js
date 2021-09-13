b3ae6a9789846b01e7f660c4f8b6404a8ddbbfa9
nathan <nathan.quellec@gmail.com>
2021-09-13 11:50:44 +0200

    update - print board

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,11 +1,34 @@
 //module
 const gameBoard = (() => {
-    let board = [3][3];
+    'use strict';
+
+    let board = [
+        ['o', 'x', 'o'],
+        ['x', 'o', 'o'],
+        ['o', 'x', 'o']
+    ];
+
 
     return {board};
+})();
+
+const displayController = (() => {
+    //const container = document.querySelector("#container");
+    const gridCase = document.querySelectorAll(".case");
+
+    const printBoardContent = () => {
+        for(let currentCase of gridCase){
+            currentCase.style.backgroundColor = "red";
+        }
+    } 
+    
+    return {printBoardContent};
 })();
 
 //factory
 const player = (name) => {
     return {name};
 };
+
+
+displayController.printBoardContent();
