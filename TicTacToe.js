9cefe945d050c743fcc6a66434b94131adb16a39
NathanQuellec <nathan.quellec@gmail.com>
2021-10-03 18:37:31 +0200

    onclick in progress

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,23 +1,31 @@
 //module
 const gameBoard = (() => {
     'use strict';
-
     let board = ['o', 'x', 'o','x', 'o', 'o','o', 'x', 'o'];
 
+    const addMarksToBoard = (marks, index) => {
+        board[index] = marks;
+    }
 
-    return {board};
+    return {board, addMarksToBoard};
 })();
 
 const displayController = (() => {
+    'use strict';
     const gridCase = document.querySelectorAll(".case");
     const currentBoard = gameBoard.board;
+
+    const clickOnCase = () => {
+
+    }
 
     const printBoardContent = () => {
 
         for(let i = 0; i < 9; i++){
             gridCase[i].innerHTML = currentBoard[i];
+            gridCase[i].addEventListener("click", clickOnCase);
         }
-    } 
+    }
     
     return {printBoardContent};
 })();
